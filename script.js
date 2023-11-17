b53e806a764b845cd1b92a5df652b21713baba21
Pritam Saha <saha7pritam@gmail.com>
2023-11-17 14:13:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const userGrid = document.getElementById('user-grid');
 
+// Display loading message
+const loadingMessage = document.createElement('p');
+loadingMessage.textContent = 'Loading user data...';
+userGrid.appendChild(loadingMessage);
+
 fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(data => {
+        userGrid.removeChild(loadingMessage);
         data.forEach(user => {
             const userElement = createUserElement(user);
             userGrid.appendChild(userElement);
         });
     })
     .catch(error => {
+        userGrid.removeChild(loadingMessage);
         console.error('Error fetching data:', error);
+        const errorMessage = document.createElement('p');
+        errorMessage.textContent = 'Failed to fetch user data. Please try again later.';
+        userGrid.appendChild(errorMessage);
     });
 
 function createUserElement(user) {
